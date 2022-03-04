5bc3d958670028805e260f18c78a38663f63f9e5
Daywick <derekli11204@gmail.com>
2022-03-04 13:53:20 -0500

    midday update

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,5 @@ console.log('connected')
 
 
 $('#test').click(function(){
-    if (document.getElementById('test').value != 'Index')
-        $('#test').text('Index');
-    else $('#test').text('Well jquery is working')
+    $('#test').text('Well jquery is working')
 })
