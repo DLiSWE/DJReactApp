838b35971fa0b62fec17e2f7687371b897e73492
Daywick <derekli11204@gmail.com>
2022-03-16 12:51:41 -0400

    test

--- stat_collector/settings/development.py
+++
@@ -1,6 +0,0 @@
-from .common import *
-
-
-DEBUG = True
-SECRET_KEY = 'django-insecure-3-)cld-wsrdrd35#g&y1%7h-%(by6qdqxodz7=fxcgp_6!bhkd'
-ALLOWED_HOSTS = ['127.0.0.1']
--- stat_collector/views.py
+++
@@ -1,4 +0,0 @@
-from django.views.generic import TemplateView, ListView
-
-class HomePage(TemplateView):
-    template_name = 'index.html'
