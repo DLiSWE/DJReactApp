480e9950e120ca3fca3f4f21cde9d6ad3d3db12f
Daywick <derekli11204@gmail.com>
2022-03-21 13:35:18 -0400

    resolve

--- stat_collector/settings/production.py
+++ stat_collector/settings/production.py
@@ -6,17 +6,3 @@ DEBUG = False
 SECRET_KEY = SECRET_KEY
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1']
-
-# DATABASES = {
-#     'default':{
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'd9k5f7ogqt4f1h',
-#         'HOST': 'ec2-18-210-191-5.compute-1.amazonaws.com',
-#         'PORT': 5432,
-#         'USER':'qyezzutaarpnjz',
-#         'PASSWORD':'dbdb32bb0107296f77f87835f301531700b5994655913831401515ffa81b2335',
-#     }
-# }
-
-
-# postgres://qyezzutaarpnjz:dbdb32bb0107296f77f87835f301531700b5994655913831401515ffa81b2335@ec2-18-210-191-5.compute-1.amazonaws.com:5432/d9k5f7ogqt4f1h
